625011897dfae4ebe6e369814aa204ac3df750df
Victor Rocha <victor.rocha2002@gmail.com>
2024-12-09 14:35:33 -0300

    Adicionando código de alocação de mem

---
+++ alocacao_de_memoria.c
@@ -0,0 +1,12 @@
+/*
+    -Uma lista consiste em uma sequencia de N itens de um mesmo tipo
+    -Lista e um tipo abstrato de dado
+        -Um TAD especifica um modelo/organizao de dados, alem de operacoes que pode ser realizadas nestes dados
+         (insercao, exclusao, busca de itens, etc)
+    -Muitas apliczcoes utilizam algum tipo de lista
+        - listas de contatos, listas de tarefas, compiladores, gerenciamento de processos, simulacoes
+    -Uma TAD lista pode ser implementado de duas formas
+        -atraves de attanjos de itens(lista estatica)
+        -atraves de alocacao dinamica(lista dinamica)
+    -EXistem muitas variacoes de lista, alem de diversas outras estruturas de dados
+*/
